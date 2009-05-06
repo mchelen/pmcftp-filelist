6308e32f546c6ce80eba05ce29e28dca0b0fb078
Michael Chelen <mike@michaelchelen.net>
2009-05-05 20:19:08 -0400

    added index.php

--- index.php
+++ index.php
@@ -17,7 +17,18 @@
 
 
     } else {
-        die($err);
+
+$myFile = "file_list.txt";
+
+$fh = fopen($myFile, 'w') or die("can't open file");
+
+$stringData = "Floppy Jalopy\n";
+fwrite($fh, $stringData);
+$stringData = "Pointy Pinto\n";
+fwrite($fh, $stringData);
+fclose($fh);
+
+
     }
 
 
